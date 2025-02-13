31041b5138af9eaa47338f769e1ec74fda843f67
Starvern <sierra.williamg@gmail.com>
2025-02-12 21:22:14 -0600

    designed 0.1.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import "../app.css"
+    import knight from '../lib/StarvernKnightPurple.png'
+</script>
+
+<main class='bg-slate-900 w-screen h-screen text-slate-200'>
+    <h1 class='text-[3rem] font-bold pl-10 pt-10'><img class='w-[4rem] mr-4 pt-1.5 float-left' alt=pfp src={knight} />William Sierra</h1>
+    <p class='text-[2rem] pl-20 mb-20'><a class='text-sky-300 underline' href="https://github.com/Starvern">GitHub Profile</a></p>
+
+    <h2 class='text-[2rem] font-semibold pl-10'>Current Public Projects:</h2>
+    <p class='text-[2rem] pl-20'>
+        <a class='text-sky-300 underline' href="https://github.com/Starvern/UltimateUserInterface">UltimateUserInterface</a>
+        A GUI library for plugin developers and server administrators for Spigot server software.
+    </p>
+
+</main>
+
